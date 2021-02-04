d9831fb5922d8fe7edcfed737c1d3130b622a61c
adan.palma <adanpalma@hotmail.com>
2021-02-03 21:00:41 -0500

    Primer commit con Pycharm

---
+++ main.py
@@ -0,0 +1,38 @@
+from math import add
+from person import Person
+
+
+def alexMethod():
+    alex = Person("alex")
+    print(alex)
+
+
+if __name__ == '__main__':
+    print("Pycharm Awsome...")
+    print("Pycharm Awsome...")
+    print("Pycharm Awsome...")
+    print("Pycharm Awsome...")
+    print("Pycharm Awsome...")
+    print("Pycharm Awsome...")
+
+    print("Pycharm Awsome...")
+    print("Pycharm Awsome...")
+    print('asdfajslfkjasldfj')
+    print("Pycharm awsome...")
+    print("%s" % "adan")
+    alexMethod()
+
+# TODO: Insert control for unreadable character
+
+print(add(5, 7))
+
+# TODO: refactor this part
+
+print("Pycharm Awsome...")
+
+name = "jamil"
+name2 = ""
+def hello(name):
+    return name
+
+
