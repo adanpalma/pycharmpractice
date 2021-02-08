bd4dac102471504fafbfe13f4aaff8cb05c26650
Adan Palma <adanpalma@hotmail.com>
2021-02-07 19:28:57 -0500

    agrega funcion remove duplicate

--- pythonChallenge.py
+++ pythonChallenge.py
@@ -1,3 +1,4 @@
+"""
 n = int(input("Input Integer"))
 
 if n % 2 != 0:
@@ -8,6 +9,18 @@ else:
     elif n >= 6:
         print("Weird")
     else:
-        print("Not Weird")
+        print("Not Weird")git
+"""
 
 
+class Solucion:
+    def removeDuplicates(self, nums):
+        nums = list(set(nums))
+        nums.sort()
+        return nums
+
+
+sol = Solucion
+nums = [1, 1, 2]
+nums = sol.removeDuplicates(sol,nums)
+print(len(nums), [x for x in nums])
